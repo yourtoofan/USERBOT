9916c6161bc7f522f94d06f9f82a71d62ed58028
Sᴜʜᴀᴀɴ ʙᴏᴛ ᴍᴀᴋᴇʀ <darkvaris6@gmail.com>
2024-11-10 20:27:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,16 +9,16 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS",7662426827 "").split()))
 OWNER_ID = int(getenv("OWNER_ID","6762113050"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://graph.org/file/c6a2ed96648fd03377dc9.jpg')
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://envs.sh/ciM.png')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/itzshukla/STRANGER-USERBOT2.0")
+REPO_URL = getenv("REPO_URL", "https://github.com/Legendbap/Shibu-userbot-")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
