652835b33c679c160750d86013e25a1ab1e6bcb3
Legend mickey <toofanpapa424@gmail.com>
2024-11-15 21:04:45 +0530

    Update dmspam.py

--- Zaid/modules/spam/dmspam.py
+++ Zaid/modules/spam/dmspam.py
@@ -5,7 +5,7 @@ import os
 import sys
 import asyncio
 from random import choice
-OWNER_ID = 6762113050
+OWNER_ID = 7520092354
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from cache.data import *
@@ -27,7 +27,7 @@ async def dmraid(xspam: Client, e: Message):
                 text = f"Chal Chal baap Ko mat sikha"
                 await e.reply_text(text)
           elif int(id) in SUDO_USERS:
-                text = f"Abe Lawde that guy part of Stranger."
+                text = f"Abe Lawde that guy part of Dev."
                 await e.reply_text(text)
           else:
               counts = int(Zaid[0])
@@ -45,7 +45,7 @@ async def dmraid(xspam: Client, e: Message):
                 text = f"Chal Chal baap Ko mat sikha"
                 await e.reply_text(text)
           elif int(id) in SUDO_USERS:
-                text = f"Abe Lawde that guy part of Stranger."
+                text = f"Abe Lawde that guy part of Dev."
                 await e.reply_text(text)
           else:
               counts = int(Zaid[0])
@@ -86,7 +86,7 @@ async def dmspam(spam: Client, e: Message):
                 text = f"Chal Chal baap Ko mat sikha"
                 await e.reply_text(text)
           elif int(id) in SUDO_USERS:
-                text = f"Abe Lawde that guy part of Stranger."
+                text = f"Abe Lawde that guy part of Devs."
                 await e.reply_text(text)
           else:
               counts = int(text[0])
