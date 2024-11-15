98cc961bc11fcce157a9b0547451951ea694f545
Legend mickey <toofanpapa424@gmail.com>
2024-11-15 21:12:43 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -38,13 +38,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "⚡️𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥⚡️"
+            ac.title = "⚡️ɪɴᴄʀɪᴄɪʙʟᴇ⚡️"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @SHIVANSH474 × @MASTIWITHFRIENDSXD •",
+                f"```{str(ac)}```\n• @llMR_VAMPIRE_KINGll × @Legend_mickey •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @SHIVANSH474"
+            this_command += "© @llMR_VAMPIRE_KINGll"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @SHIVANSH474 × @MASTIWITHFRIENDSXD •"
+            message, f"```{str(ac)}```\n• @Legend_mickey × @llMR_VAMPIRE_KINGll •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
